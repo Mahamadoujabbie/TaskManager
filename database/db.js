cba9b371064e960f03bb3175a44182bbc88b0017
yankuba susso <yankubasusso03@gmail.com>
2026-05-14 12:30:10 +0000

    connection succesfull

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
