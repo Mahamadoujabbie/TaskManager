8c4e7cc96a54851d79c309e01caa104951288ef4
mahamadou jabbie <mahamadoujabbie838@gmail.com>
2026-05-23 00:46:54 +0000

    payload

--- src/usersCreation/Oauth2.js
+++ src/usersCreation/Oauth2.js
@@ -14,23 +14,21 @@ module.exports = (Oauth2) => {
       const payload = ticket.getPayload();
       const { name, username } = payload;
       // Check if user exists in the database
-      const result = await pool.query(findUser.findByUsername, [username]);
-      let user = result.rows;
-      if (!user || user.length === 0) {
-        // If user doesn't exist, create a new user
-        await pool.query(insertUser.createUser, [
-          name,
-          username,
-          "null",
-          new Date().toISOString().split("T")[0],
-          "active",
-          "user",
-        ]);
-        return res.status(201).send({ message: "User created successfully" });
-      }
-      return res
-        .status(200)
-        .send({ message: "User authenticated successfully" });
+      // const result = await pool.query(findUser.findByUsername, [username]);
+      // let user = result.rows;
+      // if (!user || user.length === 0) {
+      //   // If user doesn't exist, create a new user
+      //   await pool.query(insertUser.createUser, [
+      //     name,
+      //     username,
+      //     "null",
+      //     new Date().toISOString().split("T")[0],
+      //     "active",
+      //     "user",
+      //   ]);
+      //   return res.status(201).send({ message: "User created successfully" });
+      // }
+      return res.status(200).send(payload); // You can customize the response as needed
     } catch (error) {
       console.error("Error verifying Google token:", error);
       return res.status(401).send({ error: "Invalid Google token" });
