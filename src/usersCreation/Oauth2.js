cbe685a82a5a839206220d92eabe1a1f69fc8911
mahamadou jabbie <mahamadoujabbie838@gmail.com>
2026-05-23 00:51:48 +0000

    username to email

--- src/usersCreation/Oauth2.js
+++ src/usersCreation/Oauth2.js
@@ -12,22 +12,22 @@ module.exports = (Oauth2) => {
       });
 
       const payload = ticket.getPayload();
-      const { name, username } = payload;
+      const { name, email } = payload;
       // Check if user exists in the database
-      // const result = await pool.query(findUser.findByUsername, [username]);
-      // let user = result.rows;
-      // if (!user || user.length === 0) {
-      //   // If user doesn't exist, create a new user
-      //   await pool.query(insertUser.createUser, [
-      //     name,
-      //     username,
-      //     "null",
-      //     new Date().toISOString().split("T")[0],
-      //     "active",
-      //     "user",
-      //   ]);
-      //   return res.status(201).send({ message: "User created successfully" });
-      // }
+      const result = await pool.query(findUser.findByUsername, [email]);
+      let user = result.rows;
+      if (!user || user.length === 0) {
+        // If user doesn't exist, create a new user
+        await pool.query(insertUser.createUser, [
+          name,
+          email,
+          "null",
+          new Date().toISOString().split("T")[0],
+          "active",
+          "user",
+        ]);
+        return res.status(201).send({ message: "User created successfully" });
+      }
       return res.status(200).send(payload); // You can customize the response as needed
     } catch (error) {
       console.error("Error verifying Google token:", error);
